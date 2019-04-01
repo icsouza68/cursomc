51eb6629ac1925d17306e48aff9b1f9a40a07a84
Ivan Souza <icsouza@yahoo.com>
2019-04-01 11:36:11 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/ivansouza/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ivansouza/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,6 @@
 package com.ivansouza.cursomc.resources;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
